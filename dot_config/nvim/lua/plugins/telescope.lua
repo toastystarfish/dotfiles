eb2a88e5703accf4db82119112fb9206209974e2
Jordan Johnson <johnson.jrdn@gmail.com>
2024-12-31 13:15:42 -0800

    Update .config/nvim/lua/plugins/telescope.lua

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -5,9 +5,16 @@ return {
   dependencies = { 'nvim-lua/plenary.nvim' },
   config = function()
     local telescope = require("telescope.builtin")
+
     vim.keymap.set('n', '<leader>ff', telescope.find_files, { desc = 'Telescope find files' })
     vim.keymap.set('n', '<leader>fg', telescope.live_grep, { desc = 'Telescope live grep' })
     vim.keymap.set('n', '<leader>fb', telescope.buffers, { desc = 'Telescope buffers' })
     vim.keymap.set('n', '<leader>fh', telescope.help_tags, { desc = 'Telescope help tags' })
+
+    require('telescope').setup({
+      defaults = {
+        file_ignore_patterns = { 'node_modules', 'target' }
+      }
+    })
   end
 }
