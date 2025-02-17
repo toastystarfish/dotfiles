de6630904cecbc38f7e1d905ed11b8c2e4133a03
Jordan Johnson <johnson.jrdn@gmail.com>
2025-02-17 10:56:23 -0800

    Update .config/nvim/lua/plugins/nvim-ts-autotag.lua

--- dot_config/nvim/lua/plugins/nvim-ts-autotag.lua
+++ dot_config/nvim/lua/plugins/nvim-ts-autotag.lua
@@ -2,11 +2,6 @@ return {
   "windwp/nvim-ts-autotag",
   lazy = false,
   config = function()
-
-    require("nvim-ts-autotag").setup({
-      enable_close = true, -- Auto close tags
-      enable_rename = true, -- Auto rename pairs of tags
-      enable_close_on_slash = false -- Auto close on trailing </
-    })
+    require("nvim-ts-autotag").setup()
   end,
 }
