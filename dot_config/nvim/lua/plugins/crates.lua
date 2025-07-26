c139c09df2e1ef5811a561219654cb0e3385b154
Jordan Johnson <johnson.jrdn@gmail.com>
2025-07-25 23:30:14 -0700

    Update .config/nvim/lua/plugins/crates.lua

--- dot_config/nvim/lua/plugins/crates.lua
+++ dot_config/nvim/lua/plugins/crates.lua
@@ -1,17 +1,7 @@
 return {
-  "Saecki/crates.nvim",
-  event = { "BufRead Cargo.toml" },
-  opts = {
-    crates = {
-      completion = {
-        enabled = true,
-      },
-    },
-    lsp = {
-      enabled = true,
-      actions = true,
-      completion = true,
-      hover = true,
-    },
-  },
+  "saecki/crates.nvim",
+  tag = "stable",
+  config = function()
+    require("crates").setup()
+  end
 }
